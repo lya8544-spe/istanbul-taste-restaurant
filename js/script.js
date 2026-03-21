9556f781fca185af41eb2a21162e90f146a75a42
lya8544-spe <lya852944@gmail.com>
2026-03-21 16:49:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -93,403 +93,402 @@ image:"https://www.tiklagelsin.com/_next/image?url=https%3A%2F%2Fcdn.tiklagelsin
 desc:"Nuggets Salata Nuggets Salata + Kutu İçecek."
 },
 
-
-  LinguineKofteMenu:{
+LinguineKofteMenu:{
 name:"Linguine Köfte Menü",
 price:"400 TL",
-image:"https://images.deliveryhero.io/image/fd-tr/Products/31839234.jpg?width=500",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839234.jpg",
 desc:"Linguine Köfte + Ayran (20 cl.)"
 },
 
 KoftePatatesMenu:{
 name:"Köfte Patates Menü",
 price:"410 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839235.jpg?width=500",
 desc:"Köfte + Patates Kızartması + Ayran (20 cl.)"
 },
 
 LinguineSchnitzelMenu:{
 name:"Linguine Schnitzel Menü",
 price:"315 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839236.jpg?width=500",
 desc:"Linguine Makarna + Schnitzel + Ayran (20 cl.)"
 },
 
 SchnitzelPatatesMenu:{
 name:"Schnitzel Patates Menü",
 price:"320 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839237.jpg?width=500",
 desc:"Schnitzel + Patates Kızartması + Ayran (20 cl.)"
 },
 
 BegendiliTavukSote:{
 name:"Beğendili Tavuk Sote",
 price:"512 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839238.jpg?width=500",
 desc:"180 gr tavuk göğüs, mantar, kapya biber, köy biberi, domates sos, beğendi sos. Linguine makarna ile"
 },
 
 SoyaSosluTavuk:{
 name:"Soya Soslu Tavuk",
 price:"390 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839239.jpg?width=500",
 desc:"180 gr tavuk göğüs, mantar, kapya biber, köy biberi, soya sos. Linguine makarna ile"
 },
 
 BarbekuSosluTavuk:{
 name:"Barbekü Soslu Tavuk",
 price:"410 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839240.jpg?width=500",
 desc:"200 gr tavuk göğüs, mantar, barbekü sos. Linguine makarna ile"
 },
 
 MantarSosluTavuk:{
 name:"Mantar Soslu Tavuk",
 price:"430 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839241.jpg?width=500",
 desc:"200 gr tavuk göğüs, mantar, krema. Linguine makarna ile"
 },
 
 KoriSosluTavuk:{
 name:"Köri Soslu Tavuk",
 price:"415 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839242.jpg?width=500",
 desc:"180 gr tavuk göğüs, mantar, kapya biber, köy biberi, köri, krema. Linguine makarna ile"
 },
 
 MeksikaSosluTavuk:{
 name:"Meksika Soslu Tavuk",
 price:"420 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839243.jpg?width=500",
 desc:"180 gr tavuk göğüs, jalapeno, pul biber, napolitan sos. Linguine makarna ile"
 },
 
 BuffaloSosluTavuk:{
 name:"Buffalo Soslu Tavuk",
 price:"420 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839244.jpg?width=500",
 desc:"180 gr tavuk göğüs, özel buffalo sos. Linguine makarna ile"
 },
 
 CheddarSosluTavuk:{
 name:"Cheddar Soslu Tavuk",
 price:"430 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839245.jpg?width=500",
 desc:"200 gr tavuk göğüs, cheddar sos. Linguine makarna ile"
 },
 
 ChipotleSosluTavuk:{
 name:"Chipotle Soslu Tavuk",
 price:"410 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839246.jpg?width=500",
 desc:"Tek kişilik chipotle soslu tavuk"
 },
 
 KekikliTavukKavurma:{
 name:"Kekikli Tavuk Kavurma",
 price:"512 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839247.jpg?width=500",
 desc:"Tek kişilik kekikli tavuk kavurma"
 },
 
 AlaturkaTavukKavurma:{
 name:"Alaturka Tavuk Kavurma",
 price:"512 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839248.jpg?width=500",
 desc:"180 gr tavuk but, sebzeler ve baharatlarla. Linguine makarna ile"
 },
 
 SweetChiliSosluTavuk:{
 name:"Sweet Chili Soslu Tavuk",
 price:"470 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839249.jpg?width=500",
 desc:"200 gr tavuk göğüs, sweet chili sos. Linguine makarna ile"
 },
 
 CafeDeParisSosluTavuk:{
 name:"Cafê De Paris Soslu Tavuk",
 price:"480 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/32299672.jpg?width=500",
 desc:"Tek kişilik Cafê De Paris soslu tavuk"
 },
 
-  MeksikaSosluDana:{
+MeksikaSosluDana:{
 name:"Meksika Soslu Dana",
 price:"680 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839250.jpg?width=500",
 desc:"160 gr dana eti, jalapeno, pul biber, napolitan sos. Linguine makarna ile"
 },
 
 MantarSosluDana:{
 name:"Mantar Soslu Dana",
 price:"690 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839251.jpg?width=500",
 desc:"170 gr dana eti, mantar ve krema sos. Linguine makarna ile"
 },
 
 AlaturkaDanaKavurma:{
 name:"Alaturka Dana Kavurma",
 price:"680 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839252.jpg?width=500",
 desc:"160 gr dana eti, sebzeler ve baharatlarla. Linguine makarna ile"
 },
 
 BegendiliMixxKofte:{
 name:"Beğendili Mixx Köfte",
 price:"522 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839254.jpg?width=500",
 desc:"160 gr köfte, beğendi sos ile. Linguine makarna ile"
 },
 
 MixxDanaAntrikot:{
 name:"Mixx Dana Antrikot",
 price:"660 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839255.jpg?width=500",
 desc:"210 gr dana antrikot. Linguine makarna ile"
 },
 
 MixxTavukBonfile:{
 name:"Mixx Tavuk Bonfile",
 price:"395 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839256.jpg?width=500",
 desc:"200 gr tavuk bonfile, özel sos ile"
 },
 
 MixxTavukPirzola:{
 name:"Mixx Tavuk Pirzola",
 price:"470 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839257.jpg?width=500",
 desc:"200 gr tavuk pirzola, özel sos ile"
 },
 
 MixxKofte:{
 name:"Mixx Köfte",
 price:"490 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839259.jpg?width=500",
 desc:"160 gr köfte. Linguine makarna ile"
 },
 
 MixxKoftePirzola:{
 name:"Mixx Köfte & Pirzola",
 price:"520 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839260.jpg?width=500",
 desc:"100 gr tavuk pirzola, 80 gr köfte. Linguine makarna ile"
 },
 
 MixxSpecialIzgara:{
 name:"Mixx Special Izgara",
 price:"540 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839262.jpg?width=500",
 desc:"Schnitzel, pirzola ve köfte karışık. Linguine makarna ile"
 },
 
 MixxSchnitzelPirzola:{
 name:"Mixx Schnitzel & Pirzola",
 price:"495 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839263.jpg?width=500",
 desc:"Tavuk pirzola ve schnitzel. Linguine makarna ile"
 },
 
 LinguinePirzola:{
 name:"Linguine Pirzola",
 price:"400 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839264.jpg?width=500",
 desc:"Özel sos, tavuk pirzola ile"
 },
 
 LinguineSchnitzel:{
 name:"Linguine Schnitzel",
 price:"395 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839265.jpg?width=500",
 desc:"Özel sos, schnitzel ile"
 },
 
 NapolitanSosluPenne:{
 name:"Napolitan Soslu Penne",
 price:"300 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839266.jpg?width=500",
 desc:"Napolitan soslu penne makarna"
 },
 
 LinguineMakarna:{
 name:"Linguine Makarna",
 price:"276 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839267.jpg?width=500",
 desc:"Özel soslu linguine makarna"
 },
 
 PenneSchnitzel:{
 name:"Penne Schnitzel",
 price:"350 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839268.jpg?width=500",
 desc:"Kremalı mantarlı sos ve schnitzel ile"
 },
 
 KofteliPenne:{
 name:"Köfteli Penne",
 price:"456 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839269.jpg?width=500",
 desc:"Kremalı mantarlı sos ve köfte ile"
 },
 
 PennePirzola:{
 name:"Penne Pirzola",
 price:"432 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839270.jpg?width=500",
 desc:"Kremalı mantarlı sos ve pirzola ile"
 },
 
 LinguineKofte:{
 name:"Linguine Köfte",
 price:"420 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839271.jpg?width=500",
 desc:"Özel sos ve köfte ile"
 },
 
 MixxManti:{
 name:"Mixx Mantı",
 price:"430 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839272.jpg?width=500",
 desc:"300 gr ev mantısı, yoğurt ve sos ile"
 },
 
 PatatesKizartmasi:{
 name:"Patates Kızartması",
 price:"204 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839282.jpg?width=500",
 desc:"Çıtır patates kızartması"
 },
 
 SoganHalkasi:{
 name:"Soğan Halkası",
 price:"210 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839283.jpg?width=500",
 desc:"8 adet çıtır soğan halkası"
 },
 
 CheddarToplari:{
 name:"Cheddar Topları",
 price:"240 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839284.jpg?width=500",
 desc:"6 adet cheddar topları"
 },
 
 ChefBasket:{
 name:"Chef Basket",
 price:"300 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839286.jpg?width=500",
 desc:"Patates, hellim, soğan halkası ve cheddar topları"
 },
 
 SpecialSalata:{
 name:"Special Salata",
 price:"410 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839273.jpg?width=500",
 desc:"Yeşillikler, cherry domates, hellim peyniri"
 },
 
 TonBalikliSalata:{
 name:"Ton Balıklı Salata",
 price:"450 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839274.jpg?width=500",
 desc:"Yeşillikler, cherry domates, ton balığı"
 },
 
 AlaturkaSalata:{
 name:"Alaturka Salata",
 price:"570 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839275.jpg?width=500",
 desc:"Yeşillikler ve cherry domates"
 },
 
 AntrikotSalata:{
 name:"Antrikot Salata",
 price:"580 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839276.jpg?width=500",
 desc:"Yeşillikler, cherry domates, antrikot"
 },
 
 DortMevsimSalata:{
 name:"4 Mevsim Salata",
 price:"290 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839277.jpg?width=500",
 desc:"Yeşillikler ve cherry domates"
 },
 
 PeynirliSalata:{
 name:"Peynirli Salata",
 price:"395 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839278.jpg?width=500",
 desc:"Yeşillikler, cherry domates, beyaz peynir"
 },
 
 HellimSalata:{
 name:"Hellim Salata",
 price:"410 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839279.jpg?width=500",
 desc:"Yeşillikler, cherry domates, hellim peyniri"
 },
 
 PirzolaSalata:{
 name:"Pirzola Salata",
 price:"400 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839280.jpg?width=500",
 desc:"Yeşillikler, cherry domates, pirzola"
 },
 
 SchnitzelSalata:{
 name:"Schnitzel Salata",
 price:"410 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/31839281.jpg?width=500",
 desc:"Yeşillikler, cherry domates, schnitzel"
 },
 
 CocaColaZero:{
 name:"Coca-Cola Zero Sugar",
 price:"84 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396809.jpg?width=500",
 desc:"33 cl kutu içecek"
 },
 
 Fanta:{
 name:"Fanta",
 price:"84 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396818.jpg?width=500",
 desc:"33 cl kutu içecek"
 },
 
 Sprite:{
 name:"Sprite",
 price:"84 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396829.jpg?width=500",
 desc:"33 cl kutu içecek"
 },
 
 FuseTea:{
 name:"Fuse Tea",
 price:"82 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396874.jpg?width=500",
 desc:"33 cl kutu içecek"
 },
 
 Ayran:{
 name:"Ayran",
 price:"76 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396910.jpg?width=500",
 desc:"20 cl ayran"
 },
 
 Su:{
 name:"Su",
 price:"50 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396916.jpg?width=500",
 desc:"50 cl su"
 },
 
 Poset:{
 name:"Poşet",
 price:"1 TL",
-image:"",
+image:"https://images.deliveryhero.io/image/fd-tr/Products/27396929.jpg?width=500",
 desc:"Plastik poşet"
 }
 };
